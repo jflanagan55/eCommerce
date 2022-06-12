4c93f4a878b782eafe4eb3e38d675681147bef2c
Justin Flanagan <jflanagan5555@gmail.com>
2022-06-12 01:25:47 -0400

    took out dom manipulation stuff

--- ecommerce/src/components/ModalForm/ModalForm.js
+++ ecommerce/src/components/ModalForm/ModalForm.js
@@ -1,117 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import * as validation from "../RegEx";
-import {connect} from 'react-redux'
+import { connect } from "react-redux";
 import { resetCart } from "../../redux/Shopping/ShoppingActions";
 
+const ModalForm = ({ totalPrice, setOpenModal, resetCart }) => {
+  const navigate = useNavigate();
 
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [address, setAddress] = useState("");
+  const [city, setCity] = useState("");
+  const [homeState, setHomeState] = useState("");
+  const [zip, setZip] = useState("");
+  const [card, setCard] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [expMonth, setExpMonth] = useState("");
+  const [expYear, setExpYear] = useState("");
+  const [error, setError] = useState("");
+  useEffect(()=>{
+   alert(error)
+  },[error])
 
-const ModalForm = ({ totalPrice, setOpenModal, resetCart} ) => {
-    const navigate = useNavigate();
-    const errorCreator = (incorrectVal) => {
-        let errorMessage = `Please enter a valid ${incorrectVal}`;
-        let errorDiv = document.createElement("div");
-        errorDiv.innerText = errorMessage;
-        errorDiv.style.color = "#cc0000";
-        errorDiv.classList.add("inputError");
-        return errorDiv;
-      };
-      
-      
-      
-      const validateForm = (event) => {
-        event.preventDefault();
-        let date = new Date()
-        let year = date.getFullYear()
-        let firstName = document.querySelector("#firstName").value;
-        let lastName = document.querySelector("#lastName").value;
-        let phoneNumber = document.querySelector("#phoneNumber").value;
-        let emailAddress = document.querySelector("#emailAddress").value;
-        let streetAddress = document.querySelector("#streetAddress").value;
-        let city = document.querySelector("#city").value;
-        let zipCode = document.querySelector("#zipCode").value;
-        let cardNumber = document.querySelector("#cardNumber").value;
-        let cvv = document.querySelector("#cvv").value;
-      
-        if (document.contains(document.querySelector(".inputError"))) {
-          document.querySelector(".inputError").remove();
-        }
-      
-        if (validation.firstNameCheck.test(firstName) === false) {
-          let error = errorCreator("first name");
-          let firstNameContainer = document.querySelector("#firstNameContainer");
-          firstNameContainer.append(error);
-        } else if (validation.lastNameCheck.test(lastName) === false) {
-          let error = errorCreator("last name");
-          let lastNameContainer = document.querySelector("#lastNameContainer");
-          lastNameContainer.append(error);
-        } else if (validation.phoneNumberCheck.test(phoneNumber) === false) {
-          let error = errorCreator("phone number");
-          let phoneContainer = document.querySelector("#phoneContainer");
-          phoneContainer.append(error);
-        } else if (validation.emailCheck.test(emailAddress) === false) {
-          let error = errorCreator("email address");
-          let emailContainer = document.querySelector("#emailContainer");
-          emailContainer.append(error);
-        } else if (validation.streetAddressCheck.test(streetAddress) === false) {
-          let error = errorCreator("street address");
-          let streetAddressContainer = document.querySelector("#streetAddressContainer");
-          streetAddressContainer.append(error);
-        } else if (validation.cityCheck.test(city) === false) {
-          let error = errorCreator("city");
-          let cityContainer = document.querySelector("#cityContainer");
-          cityContainer.append(error);
-        } else if (validation.zipCheck.test(zipCode) === false) {
-          let error = errorCreator("zip code");
-          let zipCodeContainer = document.querySelector("#zipCodeContainer");
-          zipCodeContainer.append(error);
-        } else if (validation.cardCheck.test(cardNumber) === false) {
-          let error = errorCreator("card number");
-          let cardNumberContainer = document.querySelector("#cardNumberContainer");
-          cardNumberContainer.append(error);
-        } else if (validation.cvvCheck.test(cvv) === false) {
-          let error = errorCreator("cvv/cvc");
-          let cvvContainer = document.querySelector("#cvvContainer");
-          cvvContainer.append(error);
-          
-        } else if(document.querySelector('#stateSelect').value == "blank"){
-            let errorDiv = document.createElement('div');
-            errorDiv.innerText = "Please select a State"
-            let stateContainer = document.querySelector('#stateContainer')
-            stateContainer.append(errorDiv)
-            errorDiv.style.color = "#cc0000";
-            errorDiv.classList.add("inputError");
-        }
-        else if(document.querySelector('#expMonthSelect').value === "blank" || document.querySelector('#expYearSelect').value === "blank"){
-          let errorDiv = document.createElement('div');
-          errorDiv.innerText = "Please select a Complete Expiration Date"
-          let expMonthContainer = document.querySelector('#expMonthContainer')
-          expMonthContainer.append(errorDiv)
-          errorDiv.style.color = "#cc0000";
-          errorDiv.classList.add("inputError");
-        }
-      
-        else if(parseInt(document.querySelector('#expYearSelect').value)===year){
-            let date = new Date()
-            let month = date.getMonth()
-            if(parseInt(document.querySelector('#expMonthSelect').value) < month ){
-              let errorDiv = document.createElement('div');
-              errorDiv.innerText = "Please select a Valid Expiration Date"
-              let expMonthContainer = document.querySelector('#expMonthContainer')
-              expMonthContainer.append(errorDiv)
-              errorDiv.style.color = "#cc0000";
-              errorDiv.classList.add("inputError");
-            }
-        }
-        else {
-            navigate('/orderConfirmation')
-            document.querySelector("#checkoutForm").submit();
-            resetCart();
-         
-        }
-      };
+  
+  const handleSubmit =  (e) => {
+    e.preventDefault();
+    setError("")
+    let date = new Date();
+    let currentYear = date.getFullYear();
+
+    if (validation.firstNameCheck.test(firstName) === false) {
+      setError("Invalid First Name");
+    } else if (validation.lastNameCheck.test(lastName) === false) {
+      setError("Invalid Last Name");
+    } else if (validation.phoneNumberCheck.test(phone) === false) {
+      setError("Invalid Phone Number");
+    } else if (validation.emailCheck.test(email) === false) {
+      setError("Invalid Email");
+    } else if (validation.streetAddressCheck.test(address) === false) {
+      setError("Invalid Street Address");
+    } else if (validation.cityCheck.test(city) === false) {
+      setError("Invalid City");
+    } else if (validation.zipCheck.test(zip) === false) {
+      setError("Invalid Zip Code");
+    } else if (validation.cardCheck.test(card) === false) {
+      setError("Invalid Card Number");
+    } else if (validation.cvvCheck.test(cvv) === false) {
+      setError("Invalid Cvv");
+    } else if (homeState === "") {
+      setError("Please Select a State");
+    } else if (expMonth === "" || expYear === "") {
+      setError("Please Fill in Expiration Date");
+     
+    } else if (parseInt(expYear) === currentYear) {
+      let currentMonth = date.getMonth();
+      if (parseInt(expMonth) < currentMonth) {
+        setError("Invalid Expiration Date");
+      }
+    } 
+    console.log(error)
+    // else {
+    //   navigate("/orderConfirmation");
+    //   document.querySelector("#checkoutForm").submit();
+    //   resetCart();
+    // }
+  };
   return (
-    <form className="ui form"  id="checkoutForm">
+    <form className="ui form" id="checkoutForm" onSubmit={handleSubmit}>
       <h4 className="ui dividing header">Shipping Information</h4>
       <div className="field">
         <label>Your Info</label>
@@ -122,7 +79,9 @@ const ModalForm = ({ totalPrice, setOpenModal, resetCart} ) => {
               placeholder="First Name"
               className="field"
               id="firstName"
+              value={firstName}
               required
+              onChange={(e) => setFirstName(e.target.value)}
             />
           </div>
           <div className="field" id="lastNameContainer">
@@ -131,7 +90,9 @@ const ModalForm = ({ totalPrice, setOpenModal, resetCart} ) => {
               placeholder="Last Name"
               className="field"
               id="lastName"
+              value={lastName}
               required
+              onChange={(e) => setLastName(e.target.value)}
             />
           </div>
         </div>
@@ -141,17 +102,21 @@ const ModalForm = ({ totalPrice, setOpenModal, resetCart} ) => {
               type="tel"
               placeholder="Phone Number"
               className="field"
+              value={phone}
               id="phoneNumber"
               required
+              onChange={(e) => setPhone(e.target.value)}
             />
           </div>
           <div className="field" id="emailContainer">
             <input
               type="email"
+              value={email}
               placeholder="Email Address"
               className="field"
               id="emailAddress"
               required
+              onChange={(e) => setEmail(e.target.value)}
             />
           </div>
         </div>
@@ -161,16 +126,36 @@ const ModalForm = ({ totalPrice, setOpenModal, resetCart} ) => {
         <div className=" four fields">
           <div className="field" id="streetAddressContainer">
             <label>Street Address</label>
-            <input type="text" className="field" id="streetAddress" required/>
+            <input
+              type="text"
+              className="field"
+              value={address}
+              id="streetAddress"
+              required
+              onChange={(e) => setAddress(e.target.value)}
+            />
           </div>
           <div className="field" id="cityContainer">
             <label>City</label>
-            <input type="text" className="field" id="city" required/>
+            <input
+              type="text"
+              className="field"
+              id="city"
+              value={city}
+              required
+              onChange={(e) => setCity(e.target.value)}
+            />
           </div>
           <div className="field" id="stateContainer">
             <label>State</label>
-            <select className="ui fluid dropdown" id= "stateSelect" required>
-              <option value="blank"></option>
+            <select
+              className="ui fluid dropdown"
+              value={homeState}
+              id="stateSelect"
+              required
+              onChange={(e) => setHomeState(e.target.value)}
+            >
+              <option value=""></option>
               <option value="AL">Alabama</option>
               <option value="AK">Alaska</option>
               <option value="AZ">Arizona</option>
@@ -226,7 +211,14 @@ const ModalForm = ({ totalPrice, setOpenModal, resetCart} ) => {
           </div>
           <div className="field" id="zipCodeContainer">
             <label>Zip Code</label>
-            <input type="text" className="field" id="zipCode" required/>
+            <input
+              type="text"
+              className="field"
+              id="zipCode"
+              value={zip}
+              required
+              onChange={(e) => setZip(e.target.value)}
+            />
           </div>
         </div>
         <div className="field">
@@ -234,16 +226,36 @@ const ModalForm = ({ totalPrice, setOpenModal, resetCart} ) => {
           <div className="four fields">
             <div className="field" id="cardNumberContainer">
               <label>Card Number</label>
-              <input type="text" className="field" id="cardNumber" required/>
+              <input
+                type="text"
+                className="field"
+                id="cardNumber"
+                value={card}
+                required
+                onChange={(e) => setCard(e.target.value)}
+              />
             </div>
             <div className="field" id="cvvContainer">
               <label>CVV/CVC</label>
-              <input type="text" className="field" id="cvv" required/>
+              <input
+                type="text"
+                className="field"
+                id="cvv"
+                value={cvv}
+                required
+                onChange={(e) => setCvv(e.target.value)}
+              />
             </div>
             <div className="field" id="expMonthContainer">
               <label>Exp. Month</label>
-              <select className="ui fluid search dropdown" id="expMonthSelect" required>
-                <option value="blank">Month</option>
+              <select
+                className="ui fluid search dropdown"
+                value={expMonth}
+                id="expMonthSelect"
+                required
+                onChange={(e) => setExpMonth(e.target.value)}
+              >
+                <option value="">Month</option>
                 <option value="0">January</option>
                 <option value="1">February</option>
                 <option value="2">March</option>
@@ -260,8 +272,14 @@ const ModalForm = ({ totalPrice, setOpenModal, resetCart} ) => {
             </div>
             <div className="field" id="expYearContainer">
               <label>Exp. Year</label>
-              <select className="ui fluid search dropdown" id="expYearSelect" required>
-                <option value="blank">Year</option>
+              <select
+                className="ui fluid search dropdown"
+                value={expYear}
+                id="expYearSelect"
+                required
+                onChange={(e) => setExpYear(e.target.value)}
+              >
+                <option value="">Year</option>
                 <option value="2022">2022</option>
                 <option value="2023">2023</option>
                 <option value="2024">2024</option>
@@ -287,18 +305,26 @@ const ModalForm = ({ totalPrice, setOpenModal, resetCart} ) => {
           </p>
         </div>
         <div>
-          <button className="negative ui button" type="button" onClick ={()=>setOpenModal(false)} >Back to cart</button>
-          <button className="positive ui button" type="submit" onClick={validateForm}>Checkout</button>
+          <button
+            className="negative ui button"
+            type="button"
+            onClick={() => setOpenModal(false)}
+          >
+            Back to cart
+          </button>
+          <button className="positive ui button" type="submit">
+            Checkout
+          </button>
         </div>
       </div>
     </form>
   );
 };
 
-const mapDispatchToProps = dispatch => {
-    return {
-        resetCart: ()=> dispatch(resetCart())
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    resetCart: () => dispatch(resetCart()),
+  };
+};
 
 export default connect(null, mapDispatchToProps)(ModalForm);
